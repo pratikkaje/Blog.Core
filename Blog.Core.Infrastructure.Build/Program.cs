95396f82929b559f86aadcc4aadc387e85a7bdb4
artisansol <patrickremos@artisanitsolutions.com>
2023-04-08 09:15:10 -0700

    INFRA: Generate YAML File

---
+++ Blog.Core.Infrastructure.Build/Program.cs
@@ -0,0 +1,81 @@
+﻿using ADotNet.Clients;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks.SetupDotNetTaskV1s;
+using System.Collections.Generic;
+
+namespace Blog.Core.Infrastructure.Build
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var adotNetClient = new ADotNetClient();
+
+            var githubPipeline = new GithubPipeline
+            {
+                Name = "Blog.Core build",
+
+                OnEvents = new Events
+                {
+                    Push = new PushEvent
+                    {
+                        Branches = new string[] { "master" }
+                    },
+
+                    PullRequest = new PullRequestEvent
+                    {
+                        Branches = new string[] { "master" }
+                    }
+                },
+
+                Jobs = new Jobs
+                {
+                    Build = new BuildJob
+                    {
+                        RunsOn = BuildMachines.WindowsLatest,
+
+                        Steps = new List<GithubTask>
+                        {
+                          new CheckoutTaskV2
+                          {
+                              Name = "Pulling Code"
+                          },
+
+                          new SetupDotNetTaskV1
+                          {
+                              Name = "Installing .NET",
+                              TargetDotNetVersion = new TargetDotNetVersion
+                              {
+                                  DotNetVersion = "7.0.202"
+                              }
+                          },
+
+                          new RestoreTask
+                          {
+                              Name = "Restoring .NET Packages"
+                          },
+
+                          new DotNetBuildTask
+                          {
+                              Name = "Building Solution"
+                          },
+
+                          new TestTask
+                          {
+                              Name = "Running Tests"
+                          }
+
+
+                        }
+                    }
+                }
+            };
+
+            adotNetClient.SerializeAndWriteToFile(
+                githubPipeline,
+                path:"../../../../.github/workflows/provision.yml"
+                );
+        }
+    }
+}
