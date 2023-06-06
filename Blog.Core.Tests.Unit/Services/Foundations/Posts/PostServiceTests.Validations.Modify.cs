4e788b51b2488e04968f46627909c4f5942ce733
artisansol <patrickremos@artisanitsolutions.com>
2023-06-06 20:36:44 +0400

    ShouldThrowValidationExceptionOnModifyIfPostIsInvalidAndLogItAsync -> FAIL

--- Blog.Core.Tests.Unit/Services/Foundations/Posts/PostServiceTests.Validations.Modify.cs
+++ Blog.Core.Tests.Unit/Services/Foundations/Posts/PostServiceTests.Validations.Modify.cs
@@ -44,5 +44,69 @@ namespace Blog.Core.Tests.Unit.Services.Foundations.Posts
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnModifyIfPostIsInvalidAndLogItAsync(string invalidText)
+        {
+            // given
+            Post invalidPost = new Post
+            {
+                Author = invalidText
+            };
+
+            var invalidPostException = new InvalidPostException();
+
+            invalidPostException.AddData(
+                key: nameof(Post.Id), 
+                values: "Id is required.");
+
+            invalidPostException.AddData(
+                key: nameof(Post.Content),
+                values: "Text is required.");
+
+            invalidPostException.AddData(
+                key: nameof(Post.Author),
+                values: "Text is required.");
+
+            invalidPostException.AddData(
+                key: nameof(Post.CreatedDate),
+                values: "Date is required.");
+
+            invalidPostException.AddData(
+                key: nameof(Post.UpdatedDate),
+                "Date is required."
+                ,$"Date is same as {nameof(Post.CreatedDate)}");
+
+            var expectedPostValidationException = 
+                new PostValidationException(invalidPostException);
+
+            // when
+            ValueTask<Post> modifyPostTask = 
+                this.postService.ModifyPostAsync(invalidPost);
+
+            // then
+            await Assert.ThrowsAsync<PostValidationException>(() => 
+                modifyPostTask.AsTask());
+
+            this.dateTimeBrokerMock.Verify(broker => 
+                broker.GetCurrentDateTimeOffset(), 
+                Times.Once);
+
+            this.loggingBrokerMock.Verify(broker => 
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedPostValidationException))), 
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker => 
+                broker.UpdatePostAsync(It.IsAny<Post>()), 
+                Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
