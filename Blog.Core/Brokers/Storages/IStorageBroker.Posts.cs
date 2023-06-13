0416f1b45c0dacb883c09b1fc786cea9832a5a61
artisansol <patrickremos@artisanitsolutions.com>
2023-06-13 19:45:38 +0400

    CODE RUB: Moved Post broker interface methods to correct file.

--- Blog.Core/Brokers/Storages/IStorageBroker.Posts.cs
+++ Blog.Core/Brokers/Storages/IStorageBroker.Posts.cs
@@ -1,6 +1,15 @@
-﻿namespace Blog.Core.Brokers.Storages
+﻿using Blog.Core.Models.Posts;
+using System.Linq;
+using System.Threading.Tasks;
+using System;
+
+namespace Blog.Core.Brokers.Storages
 {
     public partial interface IStorageBroker
     {
+        ValueTask<Post> InsertPostAsync(Post post);
+        IQueryable<Post> SelectAllPosts();
+        ValueTask<Post> SelectPostByIdAsync(Guid postId);
+        ValueTask<Post> UpdatePostAsync(Post post);
     }
 }
--- Blog.Core/Brokers/Storages/IStorageBroker.cs
+++ Blog.Core/Brokers/Storages/IStorageBroker.cs
@@ -6,11 +6,5 @@ using Blog.Core.Models.Posts;
 namespace Blog.Core.Brokers.Storages
 {
     public partial interface IStorageBroker
-    {
-        ValueTask<Post> InsertPostAsync(Post post);
-        IQueryable<Post> SelectAllPosts();
-        ValueTask<Post> SelectPostByIdAsync(Guid postId);
-        ValueTask<Post> UpdatePostAsync(Post post);
-
-    }
+    { }
 }
