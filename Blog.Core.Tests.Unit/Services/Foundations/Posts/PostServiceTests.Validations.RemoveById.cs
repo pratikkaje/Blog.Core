f5456db07918d72a0613682eb9a9cc293a3f19c2
artisansol <patrickremos@artisanitsolutions.com>
2023-06-14 22:58:14 +0400

    ShouldThrowValidationExceptionOnRemoveIfIdIsInvalidAndLogItAsync -> FAIL

---
+++ Blog.Core.Tests.Unit/Services/Foundations/Posts/PostServiceTests.Validations.RemoveById.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Blog.Core.Models.Posts;
+using Blog.Core.Models.Posts.Exceptions;
+using Moq;
+using Xunit;
+
+namespace Blog.Core.Tests.Unit.Services.Foundations.Posts
+{
+    public partial class PostServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnRemoveIfIdIsInvalidAndLogItAsync()
+        {
+            // given
+            Guid invalidPostId = Guid.Empty;
+
+            var invalidPostException = 
+                new InvalidPostException();
+
+            invalidPostException.AddData(
+                key: nameof(Post.Id), 
+                values: "Id is required.");
+
+            var expectedPostValidationException = 
+                new PostValidationException(invalidPostException);
+
+            // when
+            ValueTask<Post> removePostByIdTask = 
+                this.postService.RemovePostByIdAsync(invalidPostId);
+
+            // then
+            await Assert.ThrowsAsync<PostValidationException>(() => 
+                removePostByIdTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker => 
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedPostValidationException))),Times.Once);
+
+            this.storageBrokerMock.Verify(broker => 
+                broker.DeletePostAsync(It.IsAny<Post>()),
+                Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
