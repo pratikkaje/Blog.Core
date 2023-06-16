57bb650d9afb2583f8fbadf5b02de0c2cd5f22de
artisansol <patrickremos@artisanitsolutions.com>
2023-06-17 02:30:33 +0400

    CONTROLLERS: DeletePostById

--- Blog.Core/Controllers/PostsController.cs
+++ Blog.Core/Controllers/PostsController.cs
@@ -108,5 +108,43 @@ namespace Blog.Core.Controllers
                 return InternalServerError(postserviceException);
             }
         }
+
+        [HttpDelete("{postId}")]
+        public async ValueTask<ActionResult<Post>> DeletePostByIdAsync(Guid postId)
+        {
+            try
+            {
+                Post deletedPost = 
+                    await this.postService.RemovePostByIdAsync(postId);
+
+                return Ok(deletedPost);
+            }
+            catch (PostValidationException postValidationException)
+                when(postValidationException.InnerException is NotFoundPostException)
+            {
+                return NotFound(postValidationException.InnerException);
+            }
+            catch(PostValidationException postValidationException)
+            {
+                return BadRequest(postValidationException.InnerException);
+            }
+            catch(PostDependencyValidationException postDependencyValidationException)
+                when(postDependencyValidationException.InnerException is LockedPostException)
+            {
+                return Locked(postDependencyValidationException.InnerException);
+            }
+            catch(PostDependencyValidationException postDependencyValidationException)
+            {
+                return BadRequest(postDependencyValidationException);
+            }
+            catch(PostDependencyException postDependencyException)
+            {
+                return InternalServerError(postDependencyException);
+            }
+            catch(PostServiceException postserviceException)
+            {
+                return InternalServerError(postserviceException);
+            }
+        }
     }
 }
